95f4790c2d8e7f3a79409a34b7494969d6a000f4
e1k1to <eikimillian@protonmail.com>
2023-04-26 13:07:43 -0300

    foi de surfas

--- instalador.sh
+++ instalador.sh
@@ -1,10 +1,11 @@
-sudo pacman -S --needed xorg xorg-xinit libx11 libxft libxinerama xclip scrot pipewire-pulse pamixer pavucontrol mpv bc dunst yt-dlp feh xorg-setxkbmap libxrandr pacman-contrib noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-liberation ttf-anonymous-pro
+sudo pacman -S --needed xorg xorg-xinit libx11 libxft libxinerama xclip scrot pipewire-pulse pamixer pavucontrol mpv bc dunst yt-dlp feh xorg-setxkbmap libxrandr pacman-contrib noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-liberation ttf-anonymous-pro webkit2gtk gcr
 
 #!/bin/bash
 cd st && make && sudo make install && cd ..
 cd dwm && make && sudo make install && cd ..
 cd dmenu && make && sudo make install && cd ..
 cd dwmblocks && make && sudo make install && cd ..
+cd surf && make && sudo make install && cd ..
 cd configFiles && ./install.sh && cd ..
 sudo cp bins/binFolder/* /bin/.
 mkdir /home/eiki/Pictures/
