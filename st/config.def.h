934073ee4343240ef18b5e8a1fa3048b1c58eae7
e1k1to <eikimillian@protonmail.com>
2023-07-26 17:33:09 -0300

    monocraft

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Anonymous Pro:pixelsize=22antialias=true:autohint=true";
+//static char *font = "Anonymous Pro:pixelsize=22antialias=true:autohint=true";
+static char *font = "Monocraft:pixelsize=22antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
