5b16e69b84e54b80b7713daf971c61313e22f452
e1k1to <eikimillian@gmail.com>
2023-08-13 21:09:12 -0300

    burra

--- configFiles/install.sh
+++ configFiles/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Colocar arquivos de configuração automaticamente
-cp -r dunst ~/.config/.
-cp -r kitty ~/.config/.
-cp -r nvim ~/.config/.
-cp -r ranger ~/.config/.
+cp -r dunst ~/.config/
+cp -r kitty ~/.config/
+cp -r nvim ~/.config/
+cp -r ranger ~/.config/
