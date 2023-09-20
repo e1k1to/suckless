41c5eb6ec810a07de59bfc5f64ceecabdd31713e
e1k1to <eikimillian@protonmail.com>
2023-09-20 12:38:32 -0300

    corzinha st

--- st/config.def.h
+++ st/config.def.h
@@ -99,43 +99,84 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#5C5F77", //preto
+	"#ff9191", //vermelho
+	"#6eff7a", //verde
+	"#FDFD96", //amarelo
+	"#6edeff", //azul
+	"#fdc9ff", //rosa
+	"#A4D8D8", //ciano
+	"#ACB0BE", //branco
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#6C6F85", //preto
+	"#ff9191", //vermelho
+	"#6eff7a", //verde
+	"#FDFD96", //amarelo
+	//"#BDEDFB", //azul
+    "#6edeff",  //azul
+	//"#F3D7F4", //rosa
+	"#fdc9ff", //rosa
+	"#A4D8D8", //ciano
+	"#BCC0CC", //branco
 
-	[255] = 0,
+[256] = "#4C4F69", /* default foreground colour */
+[257] = "#EFF1F5", /* default background colour */
+[258] = "#DC8A78", /*575268*/
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"#222222", /* default background colour */
 };
 
 
 /*
- * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
+
+
+///* Terminal colors (16 first used in escape sequence) */
+//
+//static const char *colorname[] = {
+//	/* 8 normal colors */
+//	"black",
+//	"red3",
+//	"green3",
+//	"yellow3",
+//	"blue2",
+//	"magenta3",
+//	"cyan3",
+//	"gray90",
+//
+//	/* 8 bright colors */
+//	"gray50",
+//	"red",
+//	"green",
+//	"yellow",
+//	"#5c5cff",
+//	"magenta",
+//	"cyan",
+//	"white",
+//
+//	[255] = 0,
+//
+//	/* more colors can be added after 255 to use with DefaultXX */
+//	"#cccccc",
+//	"#555555",
+//	"gray90", /* default foreground colour */
+//	"#222222", /* default background colour */
+//};
+//
+//
+/*
+// * Default colors (colorname index)
+// * foreground, background, cursor, reverse cursor
+// */
+//unsigned int defaultfg = 258;
+//unsigned int defaultbg = 259;
+//unsigned int defaultcs = 256;
+//static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
