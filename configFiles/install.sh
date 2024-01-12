022173d210b6d212b5c62421c023e84bcaa78a59
e1k1to <eikimillian@protonmail.com>
2024-01-12 17:28:35 -0300

    instalador

--- configFiles/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Colocar arquivos de configuração automaticamente
-cp -r dunst ~/.config/
-cp -r kitty ~/.config/
-cp -r nvim ~/.config/
-cp -r ranger ~/.config/
--- instalador.sh
+++ instalador.sh
@@ -1,12 +1,14 @@
 sudo pacman -S --needed xorg xorg-xinit libx11 libxft libxinerama xclip scrot pipewire-pulse pamixer pavucontrol mpv bc dunst yt-dlp feh xorg-setxkbmap libxrandr pacman-contrib noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-liberation ttf-anonymous-pro webkit2gtk gcr gst-plugins-good gst-libav
 
 #!/bin/bash
-cd st && make && sudo make install && cd ..
-cd dwm && make && sudo make install && cd ..
-cd dmenu && make && sudo make install && cd ..
-cd slstatus && make && sudo make install && cd ..
-cd surf && make && sudo make install && cd ..
-cd configFiles && ./install.sh && cd ..
+cd st           && make && sudo make install && cd ..
+cd slock        && make && sudo make install && cd ..
+cd dwm          && make && sudo make install && cd ..
+cd dmenu        && make && sudo make install && cd ..
+cd slstatus     && make && sudo make install && cd ..
+cd surf         && make && sudo make install && cd ..
+mkdir ~/.config/
+cp -r configFiles/* ~/.config/
 sudo cp bins/binFolder/* /bin/.
 mkdir ~/Pictures/
-cp $(pwd)/adfiles/tuxx.png ~/Pictures/tuxx.png
+cp adfiles/tuxx.png ~/Pictures/tuxx.png
