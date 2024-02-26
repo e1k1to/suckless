c43214e1f16cf0c58107bfc89f2b0aa24bdafc49
e1k1to <eikimillian@protonmail.com>
2024-02-26 20:29:12 -0300

    funcionou

--- dmenu/config.h
+++ dmenu/config.h
@@ -10,7 +10,6 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-<<<<<<< HEAD
 	[SchemeNorm] ={ "#ffffff", "#000000" },
 	[SchemeSel] = { "#000000", "#ffffff" },
 	[SchemeOut] = { "#ffffff", "#000000" },
@@ -19,16 +18,6 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
     */
-=======
-	
-    [SchemeNorm] = { "#ffffff", "#000000" },
-	[SchemeSel] = { "#000000", "#ffffff" },
-	[SchemeOut] = { "#ffffff", "#000000" },
-
-    /*[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },*/
->>>>>>> 4b7de4f (updates surf)
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
